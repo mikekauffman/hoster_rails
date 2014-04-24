dcb4ceb3c9fa8afd8dda3de78a3fea233778106d
mikekauffman <mike.a.kauffman@gmail.com>
2014-04-24 14:17:15 -0600

    extracted a couple methods and fixed a bug where my if display statement wasn't functioning

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -6,31 +6,35 @@ class PartiesController < ApplicationController
   end
 
   def create
-    party = Party.new
-    party.name = params[:party_name]
-    party.size = params[:party_size]
-    party.phone = params[:party_phone]
-    party.save
+    Party.create(name: params[:party_name], size: params[:party_size], phone: params[:party_phone])
     redirect_to '/'
   end
 
   def remove_from_list
-    party = Party.find(params[:id])
-    party.update(display: false)
+    find_by_id.update(display: false)
     redirect_to '/'
   end
 
   def text
-    party = Party.find(params[:id])
-    account_sid = 'ACed7d490605ada89082d0588be5b62908'
-    auth_token = 'a34a00e7199e7c09ee448651d1013a03'
-    client = Twilio::REST::Client.new account_sid, auth_token
-    client.account.messages.create({
-                                     :from => '+12025172698',
-                                     :to => party.phone,
-                                     :body => 'Your table is ready'
-                                   })
+    send_text(find_by_id.phone)
     redirect_to '/'
+  end
+
+private
+
+  def find_by_id
+    Party.find(params[:id])
+  end
+
+  def send_text(phone)
+  account_sid = 'ACed7d490605ada89082d0588be5b62908'
+  auth_token = 'a34a00e7199e7c09ee448651d1013a03'
+  client = Twilio::REST::Client.new account_sid, auth_token
+  client.account.messages.create({
+                                   :from => '+12025172698',
+                                   :to => phone,
+                                   :body => 'Your table is ready'
+                                 })
   end
 
 end
